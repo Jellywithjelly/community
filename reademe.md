5fffa9073c8bf43b020690fd3c4b47b521d4ea42
Jellywhitjelly <m15555653836@163.com>
2020-03-01 17:04:40 +0800

    index and bootstrap

--- reademe.md
+++ reademe.md
@@ -1,2 +1,13 @@
-码农社区
-    测试追加
+## 码农社区
+## 资料
+[spring 文档](https://spring.io.guides)
+
+[spring web](https://spring.io/guides/gs/service-web-content/)
+
+[es](https://elasticsearch.cn/explore)
+
+[Github deploy key](https://developer.github.com/v3/guides/managing-deploy-keys/#deploy-keys)
+
+[Bootstrap](https://v3.bootcss.com/components/)
+
+
--- src/main/java/life/manong/community/controller/IndexController.java
+++ src/main/java/life/manong/community/controller/IndexController.java
@@ -1,15 +1,13 @@
 package life.manong.community.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
-    @GetMapping("/greeting")
-    public String index(@RequestParam("name") String name, Model model){
-        model.addAttribute("name",name);
+    @GetMapping("/")
+    public String index(){
+
         return "index";
     }
 }
